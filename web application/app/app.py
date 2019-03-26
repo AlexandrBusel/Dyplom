ae36bd0fe1aa68ace3e1bdeb900c5c28d7521ed0
Alexandr <45964982+AlexandrBusel@users.noreply.github.com>
2019-03-26 23:30:37 +0300

    Add files via upload

--- web application/app/app.py
+++ web application/app/app.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from config import Configuration
 
-from posts.blueprint import afisha
-
+from posts.afisha import afisha
+from posts.cursy import cursy
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 
 app.register_blueprint(afisha, url_prefix='/afisha')
 
-# app.register_blueprint(cursy, url_prefix='/cursy')
+app.register_blueprint(cursy, url_prefix='/cursy')
---
+++ web application/app/posts/afisha.py
@@ -0,0 +1,87 @@
+
+
+import requests
+
+from flask import Blueprint
+from flask import render_template
+from flask import request
+
+from lxml import html
+
+
+
+afisha = Blueprint('afisha', __name__, template_folder='templates')
+
+
+@afisha.route('/')
+
+
+def index():
+    datefilm = request.args.get('datefilm')
+
+    if datefilm != None:
+        movies_params = {
+        'url':'https://afisha.tut.by/day/film/{}'.format(datefilm), 
+        'current_events':'//div[@id="events-block"]',
+        'events_list':'ul[@class = "b-lists list_afisha col-5"]',
+        'film_name':'.//a[@class = "name"]/span//text()',
+        }
+    
+        tree = html.fromstring(requests.get(movies_params['url']).text)
+
+        current_events = tree.xpath(movies_params['current_events'])[0]
+        events_list = current_events.xpath(movies_params['events_list'])
+
+
+        def create_movies_list():
+            movies_list = []
+
+            for movies_block in events_list:
+
+                for movie in movies_block:
+
+                    movies_list+=[movie.xpath(movies_params['film_name'])[0]]
+
+            return movies_list
+
+
+        return render_template('movies.html', movies = create_movies_list())
+
+    else:
+
+        movies_params = {
+
+        'url':'https://afisha.tut.by/film/', 
+
+        'current_events':'//div[@id="events-block"]',
+
+        'events_list':'ul[@class = "b-lists list_afisha col-5"]',
+
+        'film_name':'.//a[@class = "name"]/span//text()',
+        }
+
+
+
+        class MoviesList:
+
+            tree = html.fromstring(requests.get(movies_params['url']).text)
+
+            current_events = tree.xpath(movies_params['current_events'])[0]
+            events_list = current_events.xpath(movies_params['events_list'])
+                
+
+            def create_movies_list(self):
+                movies_list = []
+                
+                for movies_block in self.events_list:
+                    
+                    for movie in movies_block:
+                        movies_list+=[movie.xpath(movies_params['film_name'])[0]]
+                
+                return movies_list
+
+        return render_template('movies.html' , movies = MoviesList().create_movies_list())
+
+
+
+	
