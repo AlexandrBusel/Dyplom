53e20bf0eabd79d4e0d0bf3957bce3336866c8b6
Alexandr <45964982+AlexandrBusel@users.noreply.github.com>
2019-03-31 15:45:37 +0300

    Add files via upload

--- Bot/comands.py
+++ Bot/comands.py
@@ -1,6 +1,6 @@
 
 import requests
-
+from datetime import datetime
 
 def get_weather_today():
 	city_name = "в Минске"
@@ -10,8 +10,18 @@ def get_weather_today():
 	res = requests.get("http://api.openweathermap.org/data/2.5/weather",
 	    params={'id': city_id, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
 	data = res.json()
-	return ("Сегодня:", city_name),	("состояние погоды:", data['weather'][0]['description']), ("Температура воздуха:", data['main']['temp']), ("минимальная температура:", data['main']['temp_min']), ("максимальная температура:", data['main']['temp_max'])	
 	
+	rasvet = data['sys']['sunrise']	
+	voshod = datetime.fromtimestamp(rasvet).strftime('%Y-%m-%d %H:%M:%S')
+	zakat = data['sys']['sunset']	
+	nzakat = datetime.fromtimestamp(zakat).strftime('%Y-%m-%d %H:%M:%S')
+	
+	weather = ["Сегодня "+str(city_name)+': '+str(data['weather'][0]['description'])+',   '+ 'Скорость ветра:  '+str(data['wind']['speed'])+' '+'м/с, '+"направление:  "+str(data['wind']['deg'])+',   '+"Атмосферное давление:   "+str(data['main']['pressure'])+' гПа,   '+"Влажность:   "+str(data['main']['humidity'])+'%,   '+"Температура воздуха:  "+str(data['main']['temp']) +',   '+ "минимальная температура:  "+ str(data['main']['temp_min']) +',   '+ "максимальная температура:  "+str(data['main']['temp_max'])+',   '+"восход:  "+str(voshod)+',   '+"закат:  "+str(nzakat)]
+	return weather
+	
+
+
+
 
 def get_rate_today_usd():
 	
