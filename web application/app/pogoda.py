8cecb0f062bb355900cda029c8dbd3510e83d0d7
Alexandr <45964982+AlexandrBusel@users.noreply.github.com>
2019-03-31 15:42:03 +0300

    Add files via upload

--- web application/app/pogoda.py
+++ web application/app/pogoda.py
@@ -1,6 +1,7 @@
 
 import requests
 
+from datetime import datetime
 
 def weather():
 
@@ -9,12 +10,16 @@ def weather():
 	appid = "812e89737fcf7992cc92812abee5c137"
 	
 	res = requests.get("http://api.openweathermap.org/data/2.5/weather",
-	    params={'id': city_id, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+			params={'id': city_id, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
 	data = res.json()
+
+	rasvet = data['sys']['sunrise']	
+	voshod = datetime.fromtimestamp(rasvet).strftime('%Y-%m-%d %H:%M:%S')
+	zakat = data['sys']['sunset']	
+	nzakat = datetime.fromtimestamp(zakat).strftime('%Y-%m-%d %H:%M:%S')
 	
-	weather = [str(city_name)+': '+str(data['weather'][0]['description'])+',   '+ 'Скорость ветра:'+' '+str(data['wind']['speed'])+' '+'м/с'+',   '+"направление:"+' '+str(data['wind']['deg'])+',   '+"Температура воздуха"+': '+str(data['main']['temp']) +',   '+ "минимальная температура:"+' '+ str(data['main']['temp_min']) +',   '+ "максимальная температура:"+' '+str(data['main']['temp_max'])+',   '+"восход:"+' '+str(data['sys']['sunrise'])+',   '+"закат:"+' '+str(data['sys']['sunset'])]
+	weather = [str(city_name)+': '+str(data['weather'][0]['description'])+',   '+ 'Скорость ветра:  '+str(data['wind']['speed'])+' '+'м/с, '+"направление:  "+str(data['wind']['deg'])+',   '+"Атмосферное давление:   "+str(data['main']['pressure'])+' гПа,   '+"Влажность:   "+str(data['main']['humidity'])+'%,   '+"Температура воздуха:  "+str(data['main']['temp']) +',   '+ "минимальная температура:  "+ str(data['main']['temp_min']) +',   '+ "максимальная температура:  "+str(data['main']['temp_max'])+',   '+"восход:  "+str(voshod)+',   '+"закат:  "+str(nzakat)]
 	return weather
 
 
-
-
+	
